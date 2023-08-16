93bbea1cae704e45b8137932e338853bba26acc3
Donte614 <laffytaffy641@icloud.com>
2023-08-16 15:13:55 -0400

    first commit mongodb

---
+++ views/Show.jsx
@@ -0,0 +1,17 @@
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+const React = require('react');
+class Show extends React.Component {
+    render () {
+        const fruit = this.props.fruit
+        return (
+            <div>
+            <h1>Fruits Page</h1>
+                The {fruit.name} is {fruit.color}
+                {fruit.readyToEat? 'It is good to eat' : 'Nope it is not ready yet'}
+            </div>
+        );
+    }
+}
+module.exports = Show;
